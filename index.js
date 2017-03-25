fd7e6aa69962aee9b8154dd1fe1f66bc4e558379
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-03-24 23:18:03 -0400

    feat(name): validate input package names, close #2

--- index.js
+++ index.js
@@ -3,11 +3,23 @@
 const {send, createError} = require('micro')
 const {installAndBundle} = require('./install-and-bundle')
 const {tail} = require('ramda')
+const validate = require('validate-npm-package-name')
+
+function validateNames (...names) {
+  names.forEach(name => {
+    const result = validate(name)
+    if (!result.validForNewPackages) {
+      throw new Error(`Invalid package name "${name}"`)
+    }
+  })
+}
 
 module.exports = async (req, res) => {
   try {
     const names = tail(req.url).split('&')
     console.log('packing names', names)
+    validateNames(...names)
+
     const {bundle, timings} = await installAndBundle(...names)
     res.setHeader('Content-Type', 'application/javascript')
     res.setHeader('Cache-Control', 'max-age=99999999, public, immutable')
